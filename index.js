e277c124d7376d9774c2e7dc3857ace8e747caf3
bradyrobles <you@example.com>
2020-11-24 20:55:05 -0500

    refactored routes

--- index.js
+++ index.js
@@ -1,74 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const routes = require('./routes/main');
+const passwordRoutes = require('./routes/password');
 
 // update express settings
 app.use(bodyParser.urlencoded({ extended: false})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
-
-// request - express object
-app.get('/', (request, response) =>{
-    response.send('hello world this is cool2');
-});
-
-app.get('/status', (request, response) =>{
-    //set status and payload at the same time
-    response.status(200).json({ message: 'ok', status: 200 });
-});
-
-app.post('/signup', (request, response, next) => {
-    if (Object.keys(request.body).length == 0){
-        response.status(400).json({ message: 'invalid body', status: 400 });
-    }else{
-        response.status(200).json({ message: 'ok', status: 200 });
-    }
-});
-
-app.post('/login', (request, response) => {
-    if (Object.keys(request.body).length == 0){
-        response.status(400).json({ message: 'invalid body', status: 400 });
-    }else{
-        response.status(200).json({ message: 'ok', status: 200 });
-    }
-});
-
-app.post('/logout', (request, response) => {
-    if (Object.keys(request.body).length == 0){
-        response.status(400).json({ message: 'invalid body', status: 400 });
-    }else{
-        response.status(200).json({ message: 'ok', status: 200 });
-    }
-});
-
-app.post('/token', (request, response) => {
-    if (Object.keys(request.body).length == 0 || !request.body.refreshToken){
-        response.status(400).json({ message: 'invalid body', status: 400 });
-    }else{
-        const {refreshToken} = request.body;
-        response.status(200).json({ message: `refresh token requested for token: ${refreshToken}`, status: 200 });
-    }
-    
-});
-
-app.post('/forgot-password', (request, response) => {
-    if (Object.keys(request.body).length == 0 || !request.body.email){
-        response.status(400).json({ message: 'invalid body', status: 400 });
-    }else{
-        const {email} = request.body;
-        response.status(200).json({ message: `forgot password requested for email: ${email}`, status: 200 });
-    }
-});
-
-app.post('/reset-password', (request, response) => {
-    if (Object.keys(request.body).length == 0 || !request.body.email){
-        response.status(400).json({ message: 'invalid body', status: 400 });
-    }else{
-        const {email} = request.body;
-        response.status(200).json({ message: `password reset requested for email: ${email}`, status: 200 });
-    }
-});
+// setup routes
+app.use('/', routes);
+app.use('/', passwordRoutes);
 
 // catch all other routes, 'path' param defaults to '/'
 // Middleware is executed sequentially, so order is important
