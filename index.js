a9ef7cdc9fde24a49ff581dd5ac783a00c6ee1de
bradyrobles <you@example.com>
2020-11-30 12:40:42 -0500

    added url parsing and mongocongif

--- index.js
+++ index.js
@@ -4,18 +4,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
-const app = express();
-const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 const routes = require('./routes/main');
 const passwordRoutes = require('./routes/password');
+const passport = require('passport');
+
+// setup mongoDB connection
+const uri = process.env.MONGO_CONNECTION_URL;
+const mongoConfig = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+};
+
+if (process.env.MONGO_USER_NAME && process.env.MONGO_PASSWORD){
+    mongoConfig.auth = { authSource: 'admin' };
+    mongoConfig.user = process.env.MONGO_USER_NAME;
+    mongoConfig.pass = process.env.MONGO_PASSWORD;
+
+}
+mongoose.connect(uri, mongoConfig);
+
+mongoose.connection.on('error', (error) =>{
+    console.log(error);
+    process.exit(1);
+});
+
+// setup ports
+const app = express();
+const port = process.env.PORT || 3000;
 
 // update express settings
 app.use(bodyParser.urlencoded({ extended: false})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }));[]
+
+// require passport auth
+require('./auth/auth');
 
 // setup routes
 app.use('/', routes);
@@ -33,6 +59,10 @@ app.use((error, request, response, next) => {
     response.status(error.status || 500).json({ error: error.message, status: 500});
 })
 
-app.listen(port, () =>{
-    console.log(`server is running on port: ${port}`);
+//connect to mongoose first, then start listening on port
+mongoose.connection.on('connected', () =>{
+    console.log("connected to mongo");
+    app.listen(port, () =>{
+        console.log(`server is running on port: ${port}`);
+    });
 });
