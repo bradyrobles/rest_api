7900975099c7601f14f5e50bbdf66f2abbe3d636
bradyrobles <you@example.com>
2020-12-10 13:34:27 -0500

    added

---
+++ public/assets/js/utils.js
@@ -0,0 +1,15 @@
+function signIn() {
+	console.log('signIn() works');
+}
+
+function signUp() {
+	console.log('signUp() works');
+}
+
+function forgotPassword() {
+	console.log('forgotPassword() works');
+}
+
+function resetPassword() {
+	console.log('resetPassword() works');
+}
